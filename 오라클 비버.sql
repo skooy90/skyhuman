b4bb7beed5c44e050d70510218a7454fe24f9cdc
송구영 <skooy90@gmail.com>
2025-05-13 12:34:26 +0900

    sql 최초 등록

---
+++ 오라클 비버.sql
@@ -0,0 +1,11 @@
+SELECT	* FROM emp
+ORDER BY sal DESC;
+
+SELECT * FROM emp
+ORDER BY deptno ASC, sal DESC;
+
+-- deptno asc, sal desc; 이 상황에서 연봉이 같으면 사우ㅝㄴ 번호 내림차순 정렬
+
+SELECT * FROM emp
+ORDER BY DEPTNO ASC, sal DESC, EMPNO DESC;
+
